90a983a54d4b60d703abf632f9b6292a7a343df3
zqguang3708 <940233647@qq.com>
2015-12-05 13:35:41 +0800

    机房自动.bat

---
+++ 机房自动.bat
@@ -0,0 +1,32 @@
+@echo off
+@REM 个人使用 Powered by Caesar
+@echo *******************************************************************************
+@set jifangIpHead=172.29.12.
+@set /a jifangIpEnd=1
+
+:loop
+@set /a jifangIpEnd=%jifangIpEnd%+1
+@IF jifangIpEnd==255 GOTO IPALL
+@set jifangIpReal=%jifangIpHead%%jifangIpEnd%
+@netsh interface IP set address "校园网" static %jifangIpReal% 255.255.255.0 172.29.12.1
+@if not %errorlevel% EQU 0 goto erroradmin
+@echo                                  IP OK......
+@ping /n 5 127.0.0.1 >nul
+@GOTO IPKO
+
+:IPALL
+@echo                              貌似所以可用的IP都被占用了
+@GOTO exit 
+
+:IPKO 
+@ping www.baidu.com -n 2 >nul
+@if not %errorlevel% EQU 0 goto loop
+@echo                                     BINGO!
+@GOTO exit                        
+
+:erroradmin
+@echo                       IP设置失败，可能是未使用管理员身份打开
+@GOTO exit
+
+:exit 
+@ping /n 3 127.0.0.1 >nul
